4b978e7222d8b5fe761e4db2f5397fd4276e4a8c
Vedat Aslan <vdt.aslan@outlook.com>
2022-05-17 19:56:37 +0300

    usermain düzenlendi

--- src/main/java/controller/HotelRegisterController.java
+++ src/main/java/controller/HotelRegisterController.java
@@ -1,16 +1,14 @@
 package controller;
 
-import dal.dao.UserDAO;
+import dal.dao.HotelDAO;
 import helper.ViewLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.apache.commons.validator.routines.EmailValidator;
-import pojo.User;
+import pojo.Hotel;
 
 
 //otel çalisan kayit yeri
@@ -28,8 +26,73 @@ public class HotelRegisterController extends BaseController{
     @FXML
     private TextField txt_hotelSehir;
 
+private String hotelName ;
+private String hotelIlce;
+private String hotelSehir;
+private String hotelAdres;
+
+private boolean isBlank(){
+    return hotelName.isBlank()||
+    hotelAdres.isBlank()||
+    hotelIlce.isBlank()||
+    hotelSehir.isBlank();
+
+}
+
+private void showAlert(String msg){
+    new Alert(Alert.AlertType.WARNING,msg).showAndWait();
+
+}
+private boolean validInputs(){
+
+     
+    if(isBlank()){
+        showAlert("Tüm alanlar dolu olmalıdır!");
+        return false;
+    }
+
+    return true;
+}
+
+
+
+
+
     @FXML
     void registerHotelOnClick(ActionEvent event) {
+        
+        this.hotelName = txt_HotelName.getText();
+        this.hotelAdres = txt_HotelAdres.getText();
+        this.hotelIlce = txt_HotelIlce.getText();
+        this.hotelSehir = txt_hotelSehir.getText();
+       
+
+        boolean isValid = validInputs();
+
+        if(isValid){
+
+            HotelDAO customerDAO = new HotelDAO();
+
+            Hotel hotel = new Hotel(hotelName,hotelAdres,hotelIlce,hotelSehir);
+
+            System.out.println("Hotel: "+hotel);
+
+            var result = customerDAO.insert(hotel);
+
+            if(result != 0){
+
+                showAlert("Kayıt Eklendi!");
+
+                var stage = (Stage) txt_HotelName.getScene().getWindow();
+                var loginWindow = ViewLoader.load("Login",new UserLoginController());
+                stage.setScene(new Scene(loginWindow));
+            }
+
+            else {
+                showAlert("Kayıt eklenemedi!");
+            }
+
+        }
 
     }
     
--- src/main/java/controller/UserMainController.java
+++ src/main/java/controller/UserMainController.java
@@ -1,12 +1,23 @@
 package controller;
 
 import helper.ViewLoader;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
 
 public class UserMainController extends BaseController{
+    @FXML
+    private Button btn_rezervasyon;
+    @FXML
+    private Button btn_musteri;
+
+    @FXML
+    private Button btn_yeniHotel;
 
     @FXML
     private Tab tabHotelRegister;
@@ -15,11 +26,65 @@ public class UserMainController extends BaseController{
     private TabPane tabPanel;
 
     @FXML
+    void MusteriKayit(ActionEvent event) {
+        var source = event.getSource();
+        var stage = (Stage) btn_musteri.getScene().getWindow();
+
+        if (source.equals(btn_musteri)) {
+            var customerController = new CustomerRegisterController();
+            var loginWindow = ViewLoader.load("CustomerRegister", customerController);
+            stage.setScene(new Scene( loginWindow));
+        }
+        else{
+
+        }
+
+    }
+
+    @FXML
+    void rezerveEt(ActionEvent event) {  
+        var source = event.getSource();
+        var stage = (Stage) btn_rezervasyon.getScene().getWindow();
+
+        if (source.equals(btn_rezervasyon)) {
+            var roomController = new CustomerRoomController(0);
+            var loginWindow = ViewLoader.load("CustomerRooms", roomController);
+            stage.setScene(new Scene( loginWindow));
+        }
+        else{
+            
+        }
+
+
+    }
+    @FXML
+    void YeniOtelEkle(ActionEvent event) {
+
+
+        var source = event.getSource();
+        var stage = (Stage) btn_yeniHotel.getScene().getWindow();
+
+        if (source.equals(btn_yeniHotel)) {
+            var controller = new HotelRegisterController();
+            var loginWindow = ViewLoader.load("HotelRegister", controller);
+            stage.setScene(new Scene( loginWindow));
+        }
+
+        else {
+
+            // var loginWindow = ViewLoader.load("UserLogin", new UserLoginController());
+            // stage.setScene(new Scene(loginWindow));
+        }
+
+
+
+    }
+
+    @FXML
     private void initialize(){
 
-        var node = ViewLoader.load("HotelRegister",new HotelRegisterController());
-
-        tabHotelRegister.setContent(node);
+        // var node = ViewLoader.load("HotelRegister",new HotelRegisterController());
+        // tabHotelRegister.setContent(node);
     }
 
 }
